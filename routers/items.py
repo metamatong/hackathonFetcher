5cadfd701c4c09daee5e67ba1c24760a5f75e7f9
Kyle Cheon <kcheon@my.bcit.ca>
2025-01-25 11:09:43 -0800

    feat: initiate project

---
+++ routers/items.py
@@ -0,0 +1,9 @@
+# app/routers/items.py
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
